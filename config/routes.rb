cf4a8ead994c0c4bdbcd22ef87e24612ba68f2fc
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-01-29 12:04:39 +0100

    Add Articles Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: "articles#index"
+  resources :articles, except: :index
+  # con linea 4 quitamos la redunancia de tener 2 url para index en 'articles'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
