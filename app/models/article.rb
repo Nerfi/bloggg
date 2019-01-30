daa7fc0ce4a8cfaabcded90f2aa273b3f0aa3b3a
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-01-30 18:41:44 +0100

    adding img background fro a card, for intance, not working yet,checking uploader form, working

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  #permitiendo upload a photo en el aritcle model, que es donde queremos upload the article
+  #permitiendo upload a photo en el aritcle model(como otra forma), que es donde queremos upload the article
   mount_uploader :photo, PhotoUploader
 
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,18 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+# añadiendo codigo para poder añadir una img como background de unas cartas usando claudinary service
+ process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :thumnail do
+    resize_to_fit 256, 256
+  end
+
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:30", radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
+
 end
